91420caa14ce03036782d9b46b58597c104259ab
Vincent Jugé <vincent.juge@u-pem.fr>
2025-03-17 18:42:13 +0100

    Star wars

---
+++ td/src/td07/memory.js
@@ -0,0 +1,16 @@
+const game = {};
+game.create = function(images, blank) {
+  
+};
+game.build = function(div) {
+  
+};
+
+const shuffleCards = function(length) {
+  let cards = [];
+  for(let i = 0; i < length; i++) {
+    cards.push(i);
+    cards.push(i);
+  }
+  return cards;
+};
